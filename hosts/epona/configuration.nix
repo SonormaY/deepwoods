78b6d66df96ac16a0a379f51630482a4f2f4325d
Stanislav Humeniuk <stanislav.humeniuk@outlook.com>
2026-04-21 16:12:16 +0300

    fix

--- hosts/epona/configuration.nix
+++ hosts/epona/configuration.nix
@@ -30,36 +30,40 @@ in
   # ============================================================================
   # SERVICES
   # ============================================================================
+  deepwoods = {
 
-  deepwoods.core.cloudflared = {
-    enable = true;
-    tunnelId = "4b26bc6d-4f64-4b1b-92e1-d2bb3ea1afb6";
+    core.cloudflared = {
+      enable = false;
+      tunnelId = "4b26bc6d-4f64-4b1b-92e1-d2bb3ea1afb6";
 
-    credentialsFile = config.sops.secrets.cloudflared-creds.path;
-    certificateFile = "/home/sonorma/.cloudflared/cert.pem";
+      credentialsFile = config.sops.secrets.cloudflared-creds.path;
+      certificateFile = "/home/sonorma/.cloudflared/cert.pem";
 
-    ingress = {
-      "torrent.deepwoods.website" = "http://localhost:9091";
-      "epona.deepwoods.website" = "ssh://localhost:1488";
-      "music.deepwoods.website" = "http://localhost:4533";
-      "lidarr.deepwoods.website" = "http://localhost:8686";
-      "prowlarr.deepwoods.website" = "http://localhost:9696";
+      ingress = {
+        "torrent.deepwoods.website" = "http://localhost:9091";
+        "epona.deepwoods.website" = "ssh://localhost:1488";
+        "music.deepwoods.website" = "http://localhost:4533";
+        "lidarr.deepwoods.website" = "http://localhost:8686";
+        "prowlarr.deepwoods.website" = "http://localhost:9696";
+      };
+    };
+    backend = {
+
+      torrent = {
+        enable = true;
+        downloadDir = "/var/lib/transmission/downloads";
+        credentialsFile = config.sops.secrets."transmission-creds".path;
+      };
+
+      lidarr.enable = true;
+      prowlarr.enable = true;
+    };
+
+    apps.navidrome = {
+      enable = true;
+      musicFolder = "/opt/media/music";
     };
   };
-
-  deepwoods.backend.torrent = {
-    enable = true;
-    downloadDir = "/var/lib/transmission/downloads";
-    credentialsFile = config.sops.secrets."transmission-creds".path;
-  };
-
-  deepwoods.apps.navidrome = {
-    enable = true;
-    musicFolder = "/opt/media/music";
-  };
-
-  deepwoods.backend.lidarr.enable = true;
-  deepwoods.backend.prowlarr.enable = true;
 
   system.stateVersion = "25.11";
 }
