82b1fbde408c0fb275e353f895797a02eec2f8ee
Stanislav Humeniuk <stanislav.humeniuk@outlook.com>
2026-04-25 23:23:16 +0300

    update minecraft.nix

--- modules/apps/minecraft.nix
+++ modules/apps/minecraft.nix
@@ -3,8 +3,8 @@
 let
   cfg = config.deepwoods.apps.minecraft;
   modpack = pkgs.fetchPackwizModpack {
-    url = "https://raw.githubusercontent.com/SonormaY/deepwoods/refs/heads/main/mc_fabric_pack/pack.toml";
-    packHash = "sha256-GqYabwNz6vnvxR9mXwBq26RY0Qzl7cM0sBRkokuzmdo=";
+    url = "https://raw.githubusercontent.com/SonormaY/deepwoods/refs/heads/main/modules/minecraft/mc_fabric_pack/pack.toml";
+    packHash = lib.fakeHash;
   };
 in {
   options.deepwoods.apps.minecraft = {
@@ -25,7 +25,7 @@ in {
           jre_headless = pkgs.jdk25_headless;
         };
 
-        jvmOpts = "-Xms4G -Xmx4G";
+        jvmOpts = "-Xms4096M -Xmx4096M -XX:+UnlockDiagnosticVMOptions -XX:+UnlockExperimentalVMOptions -XX:+AlwaysActAsServerClassMachine -XX:+AlwaysPreTouch -XX:+DisableExplicitGC -XX:+DoEscapeAnalysis -XX:+EagerJVMCI -XX:+EliminateLocks -XX:+EnableJVMCI -XX:+EnableJVMCIProduct -XX:+OmitStackTraceInFastThrow -XX:+OptimizeStringConcat -XX:+ParallelRefProcEnabled -XX:+PerfDisableSharedMem -XX:+RangeCheckElimination -XX:+RewriteBytecodes -XX:+RewriteFrequentPairs -XX:+SegmentedCodeCache -XX:+TrustFinalNonStaticFields -XX:+UseAES -XX:+UseAESIntrinsics -XX:+UseCodeCacheFlushing -XX:+UseCompressedOops -XX:+UseCriticalJavaThreadPriority -XX:+UseCriticalJavaThreadPriority -XX:+UseFastJNIAccessors -XX:+UseFastUnorderedTimeStamps -XX:+UseFastUnorderedTimeStamps -XX:+UseFMA -XX:+UseFPUForSpilling -XX:+UseG1GC -XX:+UseInlineCaches -XX:+UseJVMCICompiler -XX:+UseLargePages -XX:+UseLoopPredicate -XX:+UseNUMA -XX:+UseStringDeduplication -XX:+UseThreadPriorities -XX:+UseTransparentHugePages -XX:+UseTransparentHugePages -XX:+UseVectorCmov -XX:+UseVectorCmov -XX:+UseXMMForArrayCopy -XX:AllocatePrefetchStyle=3 -XX:ConcGCThreads=2 -XX:-DontCompileHugeMethods -XX:G1HeapRegionSize=8M -XX:G1HeapWastePercent=5 -XX:G1MaxNewSizePercent=40 -XX:G1MixedGCCountTarget=4 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1NewSizePercent=30 -XX:G1ReservePercent=20 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:InitiatingHeapOccupancyPercent=15 -XX:MaxGCPauseMillis=200 -XX:MaxNodeLimit=240000 -XX:MaxTenuringThreshold=1 -XX:NmethodSweepActivity=1 -XX:NodeLimitFudgeFactor=8000 -XX:NonNMethodCodeHeapSize=12M -XX:NonProfiledCodeHeapSize=194M -XX:ParallelGCThreads=8 -XX:ProfiledCodeHeapSize=194M -XX:ReservedCodeCacheSize=400M -XX:SurvivorRatio=32 -XX:ThreadPriorityPolicy=1 -Xlog:async -Dfile.encoding=UTF-8 -Djava.security.egd=file:/dev/urandom -Djline.terminal=jline.UnsupportedTerminal -Dlog4j2.formatMsgNoLookups=true -Dterminal.ansi=true -Dterminal.jline=false -Djvmci.Compiler=graal -Daikars.new.flags=true -Dusing.aikars.flags=https://mcflags.emc.gs -Dgraal.CompilerConfiguration=enterprise -Dgraal.DetectInvertedLoopsAsCounted=true -Dgraal.EnterprisePartialUnroll=true -Dgraal.InfeasiblePathCorrelation=true -Dgraal.LoopInversion=true -Dgraal.OptDuplication=true -Dgraal.SpeculativeGuardMovement=true -Dgraal.StripMineNonCountedLoops=true -Dgraal.TuneInlinerExploration=1 -Dgraal.TuneInlinerExploration=1 -Dgraal.UsePriorityInlining=true -Dgraal.Vectorization=true -Dgraal.VectorizeHashes=true -Dgraal.VectorizeSIMD=true --add-modules=jdk.incubator.vector --nogui";
 
         serverProperties = {
           server-port = 25565;
